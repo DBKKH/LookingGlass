cb649ef7b41899b85613e970b1eb6d55da151c66
koike.hideki <koike.hideki@synamon.jp>
2018-12-29 12:54:16 +0900

    First

---
+++ Assets/MINE/FrontRollMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrontRollMover : MonoBehaviour
+{
+	public Vector3 rotation;
+	public Space space;
+
+	void Update()
+	{
+		float d = Time.deltaTime;
+		transform.Rotate(rotation * d, space);
+	}
+}
+
---
+++ Assets/MINE/RollMover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollMover : MonoBehaviour
+{
+	public Vector3 rotation;
+	public Space space;
+
+	void Update()
+	{
+		float d = Time.deltaTime;
+		transform.Rotate(rotation * d, space);
+	}
+}
